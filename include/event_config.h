8db3a90dee625123d6a5ba133b0d47e45360529f
ayswarya-nachinarkinian <ayswaryanachinarkinian@gmail.com>
2026-09-19 07:02:19 +0530

    Create event_config.h

---
+++ include/event_config.h
@@ -0,0 +1,13 @@
+/*
+ * event_config.h
+ *
+ *  Created on: 18-Sep-2026
+ *      Author: Ahil
+ */
+
+#ifndef INCLUDE_EVENT_CONFIG_H_
+#define INCLUDE_EVENT_CONFIG_H_
+
+
+
+#endif /* INCLUDE_EVENT_CONFIG_H_ */
